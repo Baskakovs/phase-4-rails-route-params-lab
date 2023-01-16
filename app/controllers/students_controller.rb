53151b3a53ecd50665b7faa6ed79709e38a93232
Baskakovs <baskakovs@me.com>
2023-01-16 12:23:42 +0000

    commit tree 60463cee66957cf6bf7059e2fb544ff295f1c005

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+
+    if params.keys.length > 2
+      student = Student.search(params[:name])
+      render json: student
+      else
+        student = Student.all
+        render json: student
+      end
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
